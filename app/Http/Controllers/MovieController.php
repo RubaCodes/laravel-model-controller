439c16066721b5bca91b088accb69f0a3b871728
RubaCodes <rubacodes@gmail.com>
2022-07-11 13:46:09 +0200

    fetch movie e return view

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,5 +9,10 @@ use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public funtion index(){
+
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+    }
 }
