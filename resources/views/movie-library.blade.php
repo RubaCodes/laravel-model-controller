77a4717f2c8f8ca8fc55c6922de7d93a715103c2
RubaCodes <rubacodes@gmail.com>
2022-07-11 14:54:56 +0200

    push finale

--- resources/views/movie-library.blade.php
+++ resources/views/movie-library.blade.php
@@ -7,14 +7,14 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Movie Library</title>
 </head>
-<body class="h-screen">
-    <main class="bg-blue-500 h-full">
-        <h1 class="text-4xl uppercase text-center py-8">Cineteca Boolean</h1>
+<body class="h-screen text-center ">
+    <main class="bg-gradient-to-r from-violet-500 to-fuchsia-500 h-full">
+        <h1 class="text-4xl uppercase px-8 py-4 my-8 bg-white rounded-lg inline-block shadow-md shadow-slate-800  ">Cineteca Boolean</h1>
         <div class="container mx-auto ">
             <section class="grid grid-cols-5 gap-4   rounded-lg">
                 @foreach ($movies as $movie)
-                <div class="card  border border-black bg-white rounded p-4 flex flex-col justify-between box-shadow ">
-                    <h2 class="text-xl font-bold">{{ $movie->title }}</h2>
+                <div class="card  border border-black bg-white/70 rounded-lg p-4 flex flex-col justify-between shadow-md shadow-slate-800 ">
+                    <h2 class="text-lg font-bold ">{{ $movie->title }}</h2>
                     <h3 class="pb-4">{{ $movie->original_title }}</h2>
                         <div>
                             <span> Anno: {{ date('Y',strtotime($movie->date)) }}</span>
